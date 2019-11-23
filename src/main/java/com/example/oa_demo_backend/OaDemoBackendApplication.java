d979b0172f7cc11a22102bff03237764ac2df0a3
hannah <hannah@hannahdeMacBook-Pro.local>
2019-11-23 09:06:49 +0800

    Initial commit

---
+++ src/main/java/com/example/oa_demo_backend/OaDemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.oa_demo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaDemoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaDemoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oa_demo_backend/OaDemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oa_demo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaDemoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
